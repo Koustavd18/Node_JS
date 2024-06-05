438e7745ba090099758446f97a258e483ae0896f
Koustav Das <kdkdas8@gmail.com>
2024-06-05 12:15:54 +0530

    Updates

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const http = require("http");
+const express = require("express");
 const routes = require("./routes");
 
+const app = express();
 const server = http.createServer(routes);
 
 server.listen(3000, () => {
   console.log("Server started sucessfully");
+});
+
+app.listen(3001, () => {
+  console.log("Server started at PORT 3000");
 });
